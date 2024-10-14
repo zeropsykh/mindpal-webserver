371dfd440507ab0d7617b40a8b95a60543a2d082
Adithyan K A <47267209+zeropsykh@users.noreply.github.com>
2024-10-14 08:11:50 +0530

    Update mindpal.py

--- mindpal.py
+++ mindpal.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from config import Config
 
 app = Flask(__name__)
 
